a1c2fa64b9e7b83db97c7d28c36ecd5a8194e435
yasirmukhtar <yasirmukhtar@192.168.0.13>
2014-09-20 09:34:43 -0500

    New Entities Added

---
+++ jobCommon/src/main/java/com/mum/jobportal/domain/Administrator.java
@@ -0,0 +1,11 @@
+package com.mum.jobportal.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Administrator extends Person implements Serializable{
+	
+
+}
---
+++ jobCommon/src/main/java/com/mum/jobportal/domain/JobSeeker.java
@@ -0,0 +1,8 @@
+package com.mum.jobportal.domain;
+
+import javax.persistence.Entity;
+
+@Entity
+public class JobSeeker extends Person {
+	
+}
