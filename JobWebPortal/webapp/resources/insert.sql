ba3a41c6adc18af4ac2a5bbb2dffb90fe17eeb9b
yasirmukhtar <yasirmukhtar@192.168.0.13>
2014-09-20 21:09:24 -0500

    fixed login log out

---
+++ JobWebPortal/webapp/resources/insert.sql
@@ -0,0 +1,4 @@
+INSERT INTO `jobportal`.`User` (`userName`, `enabled`, `password`) VALUES ('yasir', true, 'yasir');
+INSERT INTO `jobportal`.`User` (`userName`, `enabled`, `password`) VALUES ('awais', true, 'awais');
+INSERT INTO `jobportal`.`Authorities` (`user_role_id`, `role`, `username`) VALUES ('1', 'ROLE_USER', 'yasir');
+INSERT INTO `jobportal`.`Authorities` (`user_role_id`, `role`) VALUES ('2', 'ROLE_USER');
