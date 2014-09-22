adbbbf398b3d38b2ef83a88b1660874beda6bb19
atariq <awais_join@yahoo.com>
2014-09-22 18:06:20 -0500

    Add Category done

--- JobWebPortal/src/main/java/com/mum/jobprotal/web/controllers/HomeController.java
+++ JobWebPortal/src/main/java/com/mum/jobprotal/web/controllers/HomeController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 
 import org.apache.log4j.Logger;
 import org.springframework.security.access.annotation.Secured;
@@ -11,10 +13,14 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.mum.jobportal.domain.Authorities;
 import com.mum.jobportal.domain.Category;
+import com.mum.jobportal.domain.Employer;
 import com.mum.jobportal.service.IJobPortalService;
 import com.mum.jobportal.utils.JobPortalAuthorities;
 
@@ -32,6 +38,7 @@ public class HomeController {
 		if(request.isUserInRole(JobPortalAuthorities.ROLE_ADMIN)){
 			List<Category> catList=service.getAllCategory();
 			model.addAttribute("catList", catList);
+			model.addAttribute("category", new Category());
 			return "adminHome";
 		}
 		return "home";
@@ -43,5 +50,18 @@ public class HomeController {
 		List<Category> catList=service.getAllCategory();
 		model.addAttribute("catList", catList);
 		return "allCategories";
+	}
+	@Secured(JobPortalAuthorities.ROLE_ADMIN)
+	@RequestMapping(value="/saveCategory",method=RequestMethod.POST)
+	public String saveCategories(Model model,@Valid Category category,BindingResult result){
+
+		if(result.hasFieldErrors()){
+			model.addAttribute("catList", service.getAllCategory());
+			return "adminHome";
+		}else{
+			//Add categories
+			service.createCategory(category);
+		}
+		return "redirect:/home";
 	}
 }
--- JobWebPortal/webapp/WEB-INF/views/adminHome.jsp
+++ JobWebPortal/webapp/WEB-INF/views/adminHome.jsp
@@ -1,6 +1,7 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1" pageEncoding="ISO-8859-1"%>
 <%@ taglib uri="http://www.springframework.org/security/tags" prefix="sec" %>
-<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ taglib uri="http://www.springframework.org/tags/form" prefix="form"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <!DOCTYPE html>
 <html>
 <head>
@@ -16,7 +17,31 @@
   <div class="panel-heading">All Categories</div>
   <div class="panel-body">
     <p>
-    
+    		<form:form class="form-horizontal" role="form" commandName="category" action="saveCategory" method="post">
+				<div class="form-group">
+					<label for="title" class="col-sm-2 control-label">Title:</label>
+					<div class="col-sm-7">
+						<form:input type="text" cssClass="form-control" id="title" path="title" placeholder="Title"/>
+					</div>
+					<div class="col-sm-3">
+						<form:errors path="title" cssClass="error" />
+					</div>
+				</div>
+				<div class="form-group">
+					<label for="description" class="col-sm-2 control-label">Description:</label>
+					<div class="col-sm-7">
+						<form:input type="text" cssClass="form-control" id="description" path="description" placeholder="Description"/>
+					</div>
+					<div class="col-sm-3">
+						<form:errors path="description" cssClass="error" />
+					</div>
+				</div>
+				<div class="form-group">
+				<div class="col-sm-offset-2 col-sm-10">
+					<button type="submit" class="btn btn-default">Save</button>
+				</div>
+			</div>
+			</form:form>
     </p>
   </div>
   <!-- Table -->
